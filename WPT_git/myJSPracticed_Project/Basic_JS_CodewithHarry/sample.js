2f4c5caa15bb149c8d45f1c29153ae3443358eae
vaibhavverulkar08 <vaibhavverulkar08@gmail.com>
2023-12-30 18:16:53 +0530

    first commit WPT

---
+++ WPT_git/myJSPracticed_Project/Basic_JS_CodewithHarry/sample.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+document.write("This output comes from script tag if script is in html file / from separte js file using document write");
+console.warn("This Warning in console");
+console.error("This is error in console");
+console.log(1+1+"nd console Log");
+
